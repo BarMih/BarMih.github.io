7e9c68c6c53fdc4f190ed2b93eb5bab9bdbf275b
BarMih <79800464+BarMih@users.noreply.github.com>
2024-03-31 13:09:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('IT').addEventListener('click', () => {
+  document.querySelector('#section_IT').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('SOLID').addEventListener('click', () => {
+  document.querySelector('#section_solid').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('YAGNI_KISS_DRY').addEventListener('click', () => {
+  document.querySelector('#section_YKD').scrollIntoView({ behavior: 'smooth' });
+});
